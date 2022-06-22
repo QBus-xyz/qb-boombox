4804f3b03194fcd36256f59b6e2fd289429a59ae
Chxttr <49021789+Chxttr@users.noreply.github.com>
2022-06-22 20:58:57 +0200

    Fix for new QBCore

--- server/server.lua
+++ server/server.lua
@@ -1,5 +1,4 @@
-QBCore = nil
-TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
+local QBCore = exports['qb-core']:GetCoreObject()
 
 QBCore.Functions.CreateUseableItem("boombox", function(source, item)
     local src = source
